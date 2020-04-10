9d8bf303f0c52205bdd1ce20183c6ee1e3d94d4f
Goy288 <44177200+Goy288@users.noreply.github.com>
2020-04-10 14:54:34 -0700

    Add project files.

---
+++ My Special Access Funny Picture And Video Collection/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace My_Special_Access_Funny_Picture_And_Video_Collection.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ My Special Access Funny Picture And Video Collection/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace My_Special_Access_Funny_Picture_And_Video_Collection.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
