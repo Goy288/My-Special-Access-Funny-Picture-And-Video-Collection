ccf1fef2b807457979ec13c0d6c1a50f0e732eb0
Goy288 <44177200+Goy288@users.noreply.github.com>
2020-04-21 11:16:33 -0700

    Made media class.

---
+++ My Special Access Funny Picture And Video Collection/Models/Media.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace My_Special_Access_Funny_Picture_And_Video_Collection.Models
+{
+    public class Media
+    {
+
+        private static readonly string[] VideoTypes = new string[] { "mp4", "webm", "ogg" };
+        private static readonly string[] ImageTypes = new string[] { "jpeg", "jpg", "png", "gif" };
+
+        [Key]
+        public int MediaID { get; set; }
+
+        [Required]
+        [Column(TypeName = "NVARCHAR(260)")]
+        [StringLength(260)]
+        public string FileName { get; set; }
+
+        [Required]
+        [StringLength(64)]
+        public string FileType { get; set; }
+
+        [Required]
+        public bool IsVideo { get; set; }
+    }
+}
