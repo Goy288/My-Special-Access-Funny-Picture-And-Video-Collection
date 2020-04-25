454c484b3ea210b34aef0ba50468b192a24fec76
Goy288 <44177200+Goy288@users.noreply.github.com>
2020-04-24 23:15:32 -0700

    Made it so that you can register your own username.

---
+++ My Special Access Funny Picture And Video Collection/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using My_Special_Access_Funny_Picture_And_Video_Collection.Data;
+
+[assembly: HostingStartup(typeof(My_Special_Access_Funny_Picture_And_Video_Collection.Areas.Identity.IdentityHostingStartup))]
+namespace My_Special_Access_Funny_Picture_And_Video_Collection.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
