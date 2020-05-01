7145d9cd2f47d677136ae034d1d164c552193710
Goy288 <44177200+Goy288@users.noreply.github.com>
2020-04-30 23:31:42 -0700

    SQL script for the existing media files

---
+++ My Special Access Funny Picture And Video Collection/SQL Test Scripts/dbo.Media.data.sql
@@ -0,0 +1,8 @@
+SET IDENTITY_INSERT [dbo].[Media] ON
+INSERT INTO [dbo].[Media] ([MediaID], [FileName], [FileType], [IsVideo]) VALUES (2, N'fedora', N'jpg', 0)
+INSERT INTO [dbo].[Media] ([MediaID], [FileName], [FileType], [IsVideo]) VALUES (3, N'mmmm', N'jpg', 0)
+INSERT INTO [dbo].[Media] ([MediaID], [FileName], [FileType], [IsVideo]) VALUES (4, N'uncropped', N'jpg', 0)
+INSERT INTO [dbo].[Media] ([MediaID], [FileName], [FileType], [IsVideo]) VALUES (5, N'cat powering up v2', N'mp4', 1)
+INSERT INTO [dbo].[Media] ([MediaID], [FileName], [FileType], [IsVideo]) VALUES (6, N'smack', N'mp4', 1)
+INSERT INTO [dbo].[Media] ([MediaID], [FileName], [FileType], [IsVideo]) VALUES (7, N'wario dies', N'mp4', 1)
+SET IDENTITY_INSERT [dbo].[Media] OFF
