2e205643b9b4cf8b974b0d1b0b6b6899beb31ca7
Goy288 <44177200+Goy288@users.noreply.github.com>
2020-04-30 23:27:56 -0700

    Added constructors & GetFileUrl()

--- My Special Access Funny Picture And Video Collection/Models/Media.cs
+++ My Special Access Funny Picture And Video Collection/Models/Media.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace My_Special_Access_Funny_Picture_And_Video_Collection.Models
 {
     public class Media
     {
-
         private static readonly string[] VideoTypes = new string[] { "mp4", "webm", "ogg" };
         private static readonly string[] ImageTypes = new string[] { "jpeg", "jpg", "png", "gif" };
 
@@ -27,5 +23,48 @@ namespace My_Special_Access_Funny_Picture_And_Video_Collection.Models
 
         [Required]
         public bool IsVideo { get; set; }
+
+        public Media(string fileName)
+        {
+            string[] fileNamePieces = fileName.Split('.');
+
+            FileName = fileNamePieces[0];
+            FileType = fileNamePieces[1].ToLower();
+
+            IsVideo = false;
+            foreach (string videoType in VideoTypes)
+            {
+                if (videoType == FileType)
+                {
+                    IsVideo = true;
+                    break;
+                }
+            }
+            if (!IsVideo)
+            {
+                bool isAnyType = false;
+                foreach (string imageType in ImageTypes)
+                {
+                    if (imageType == FileType)
+                    {
+                        isAnyType = true;
+                        break;
+                    }
+                }
+                if (!isAnyType)
+                {
+                    throw new ArgumentException("This file type is not compatible.");
+                }
+            }
+        }
+        public Media() { }
+
+        public string GetFileUrl()
+        {
+            string url = "/meme/";
+            url += IsVideo ? "vid/" : "img/";
+            url += FileName + "." + FileType;
+            return url;
+        }
     }
 }
