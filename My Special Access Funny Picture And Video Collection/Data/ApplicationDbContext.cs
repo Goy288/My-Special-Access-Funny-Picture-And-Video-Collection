81c1a52a92c7af5d872b5ac16b7d147aabb99f6b
Goy288 <44177200+Goy288@users.noreply.github.com>
2020-04-30 23:28:54 -0700

    Added media to the context

--- My Special Access Funny Picture And Video Collection/Data/ApplicationDbContext.cs
+++ My Special Access Funny Picture And Video Collection/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using My_Special_Access_Funny_Picture_And_Video_Collection.Models;
 
 namespace My_Special_Access_Funny_Picture_And_Video_Collection.Data
 {
@@ -12,5 +13,6 @@ namespace My_Special_Access_Funny_Picture_And_Video_Collection.Data
             : base(options)
         {
         }
+        public DbSet<Media> Media { get; set; }
     }
 }
